203a8458cd91d874cf2f816b4d00441a09e542b8
MonsterBasket <blazza1@hotmail.com>
2022-08-18 14:59:15 +1000

    test complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react"; 
+
+// const textState = {"OFF":"ON", "ON":"OFF"}
+// let text = "OFF"
+
+// function Toggle() {
+// const [count, setCount] = useState(0)
+
+// function toggle(){
+//   setCount(count + 1);
+//   text = textState[text]
+// }
+
+//   return <button onClick={toggle}>{text}</button>;
+// }
+// Thought my attempt was pretty short and concise, the example is much better >_<
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  const color = isOn ? "red" : "white";
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  return (
+    <button style = {{background: color}} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
